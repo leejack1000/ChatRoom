d86ce7e700a1ae0e7c133ec031ec373675223a7b
Leejack-PC\Leejack <leejack1000@gmail.com>
2018-03-04 22:58:20 +0800

    [Add]Client chat code [Add]Server chat code

---
+++ Client/Assets/Example_Socket/Example_Socket.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Example_Socket : MonoBehaviour
+{
+    public string ip = "127.0.0.1";
+    public int port = 8888;
+
+    private SocketMgr mSocketMgr;
+
+    void Start()
+    {
+        mSocketMgr = new SocketMgr();
+    }
+
+    public void OnClickConnect()
+    {
+        Debug.Log("OnClickConnect");
+        mSocketMgr.Connect(ip, port);
+    }
+
+    public void OnClickClose()
+    {
+        mSocketMgr.Close();
+    }
+
+    public void OnClickSend()
+    {
+        Debug.Log("OnClickSend");
+        mSocketMgr.SendServer("{Test:123456}");
+    }
+}
---
+++ Server/Server/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+
+namespace Server
+{
+    class Program
+    {
+        private const int port = 8888;
+
+        //MAIN
+        static void Main(string[] args)
+        {
+            Server s = new Server();
+            s.initialize(port);
+        }
+    }
+
+    class Server
+    {
+        private static int numThreads = 0;
+        private byte[] readBuffer = new byte[1024];
+        private Socket server;
+        private Socket client;
+
+        public Server()
+        {
+            server = new Socket(SocketType.Stream, ProtocolType.Tcp);
+        }
+
+        public void initialize(int port)
+        {
+            IPEndPoint endpoint = new IPEndPoint(IPAddress.Any, port);
+            server.Bind(endpoint);
+            Console.Title = "Server";
+            Console.WriteLine("initialize");
+            while (true)
+            {
+                server.Listen(1);
+                client = server.Accept();
+                Thread randomClient = new Thread(readFromSocket);
+                randomClient.Name = "Thread #" + numThreads;
+                randomClient.Start();
+                ++numThreads;
+            }
+        }
+
+        public void readFromSocket()
+        {
+            while (true)
+            {
+                try
+                {
+                    int amount = client.Receive(readBuffer);
+                    if (amount > 0)
+                    {
+
+                        //string toPrint = byteToString(readBuffer);
+                        string toPrint = Encoding.UTF8.GetString(readBuffer);
+                        //toPrint = toPrint.Replace("\0", string.Empty);
+                        Console.WriteLine(toPrint);
+                        Array.Clear(readBuffer, 0, readBuffer.Length);
+                    }
+                    else
+                    {
+                        continue;
+                    }
+                }
+                catch (SocketException e)
+                {
+                    Console.WriteLine("Reading was unsuccessful");
+                    Console.WriteLine(e.Message);
+                    Console.Read();
+                }
+            }
+        }
+    }
+}
